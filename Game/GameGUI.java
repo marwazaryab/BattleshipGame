8cf14b5bac4866928be770fa06d9447ba8135d0d
marwazaryab <118633267+marwazaryab@users.noreply.github.com>
2024-01-08 19:39:38 -0500

    first commit

---
+++ Game/GameGUI.java
@@ -0,0 +1,8 @@
+package Game;
+
+import javax.swing.*;
+
+public class GameGUI extends JPanel {
+    
+    
+}
---
+++ Game/Startup.java
@@ -0,0 +1,11 @@
+package Game;
+
+public class Startup {
+
+    GameGUI mainGui;
+    MainObject mainModel;
+    
+    public static void main(String [] args){
+
+    }
+}
